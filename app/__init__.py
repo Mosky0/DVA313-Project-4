3488914269701d19daa034120a252f728253d520
Daniel Hanebring <d.hanebring@gmail.com>
2025-11-24 09:37:22 +0100

    added websockets

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.BaseConfig")
+    socketio = SocketIO(app, async_mode='eventlet')
 
     from app.routes.main import api
     app.register_blueprint(api)
